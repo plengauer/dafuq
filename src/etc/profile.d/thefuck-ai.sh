11cc5fe25e940323609418276ec5e94ba7751263
plengauer <p.lengauer@gmail.com>
2023-12-24 15:07:56 +0000

    initial complete package

--- src/etc/profile.d/thefuck-ai.sh
+++ src/etc/profile.d/thefuck-ai.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+_fuck__print_error() {
+  echo "Zero fucks given! $*" >&2
+}
+
 _fuck__fix_command() {
-  local command="$*"
+  local command="$(cat)"
   local openai_token="$(cat /opt/thefuck-ai/OPENAI_API_TOKEN)"
   local system_prompt="$(cat /var/lib/thefuck-ai/system.prompt)"
   curl https://api.openai.com/v1/chat/completions -H "Authorization: Bearer $openai_token" -H "Content-Type: application/json" -X POST -d \
@@ -10,16 +14,20 @@ _fuck__fix_command() {
 }
 
 _fuck__suggest_command() {
-  echo "$*"
+  ### strategy 0
+  # cat
+  ### strategy 1
+  # READLINE_LINE="$(cat)"
+  # READLINE_POINT=${#READLINE_LINE}
+  ### strategy 2
 }
 
 fuck() {
   local exit_code=$?
-  if [ "$exit_code" -eq 0 ]; then echo "Zero fucks given! The last command was successful."; return 0; fi
-  if [ "$-" = *"i"* ]; then echo "Zero fucks fiven! Shell is not interactive."; return 1; fi
+  if [ "$exit_code" -eq 0 ]; then _fuck__print_error "The last command was successful."; return 0; fi
+  if [ "$-" = *"i"* ]; then _fuck_print_error "Shell is not interactive."; return 1; fi
   local command="$(history | tail -n 2 | head -n 1 | awk '{$1=""; print substr($0,2)}')"
-  if [ -z "$command" ]; then echo "Zero fucks given! The command could not be fixed."; return 0; fi
-  local fixed="$(_fuck__fix_command "$command")"
-  _fuck__suggest_command "$fixed"
+  if [ -z "$command" ]; then _fuck__print_error "The command could not be fixed."; return 1; fi
+  history | tail -n 2 | head -n 1 | awk '{$1=""; print substr($0,2)}') | _fuck__fix_command | _fuck__suggest_command
   return $?
 }
